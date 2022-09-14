6db44bb7a0160df4dcf0a735e128c957e038d180
AJen01 <71494089+AJen01@users.noreply.github.com>
2022-09-15 01:49:27 +0800

    growth rates code + test data

--- DataProcessor/data/growth_rates.py
+++ DataProcessor/data/growth_rates.py
@@ -1,98 +1,95 @@
+import imp
 import os
 import numpy as np
 from natsort import natsorted
 import pandas as pd
+from pathlib import Path
+from DataProcessor.data.calc_data import Calculate as calculator
 
 class Run:
 
     def __init__(self):
         self.method = 0
+        
 
     def creates_rates_folder(self, path):
-        rates_folder = Path(path) / 'CrystalAspects' + '/GrowthRates/'
+        '''This method returns a new folder to save data,
+        with a selected CG simulation / Crystal Aspects folder'''
+
+        rates_folder = Path(path) / 'CrystalAspects' / 'GrowthRates'
         rates_folder.mkdir(parents=True, exist_ok=True)
 
         return rates_folder
 
-    def find_size(self, folders, path):
+    def find_info(self, path):
+        '''The method returns the crystallographic directions,
+        supersations, and the size_file paths from a CG simulation folder'''
+
+        path = Path(path)
         files = os.listdir(path)
-        contents = natsorted(os.listdir(path))
+        contents = natsorted(files)
         folders = []
         for item in contents:
-            path = path + '/' + item
-            if os.path.isdir(path):
-                if item.endswith('XYZ_files') or item.endswith('CrystalAspects') or item.endswith('CrystalMaps'):
+            item_name = item
+            print(item_name)
+            item_path = path / item
+            if os.path.isdir(item_path):
+                if item_name.endswith('XYZ_files') or item_name.endswith('CrystalAspects') \
+                or item_name.endswith('CrystalMaps'):
                     continue
                 else:
-                    folders.append(item)
-        file = []
-        for i in range(len(folders)):
-            path = path + '/' + folders[i]
-            files = os.listdir(path)
-            for f in files:
-                if f.startswith("._"):
-                    continue
-                if f.endswith('size.csv'):
-                    file.append(f)
-                    file_path = folderpath + '/' + f
-                    # print(file_path)
-        return folders, file
+                    folders.append(item_path)
+        print(folders)
+        size_files = []
+        supersats = []
 
-    def find_supersat(self, path, folders):
-        files = os.listdir(path)
-        supersaturation = []
-        for i in range(len(folders)):
-            path = path + '/' + folders[i]
-            files = os.listdir(path)
+        for folder in folders:
+            files = os.listdir(folder)
             for f in files:
-                if f.startswith("._"):
+                f_path = path / folder / f
+                f_name = f
+                if f_name.startswith("._"):
                     continue
-                if f.endswith('simulation_parameters.txt'):
-                    filepath = folderpath + '/' + folders[i] + '/' + f
-                    # print(filepath)
-                    with open(filepath, 'r') as sim_file:
+                if f_name.endswith('size.csv'):
+                    size_files.append(f_path)
+            
+                if f_name.endswith('simulation_parameters.txt'):
+                    with open(f_path, 'r', encoding='utf-8') as sim_file:
                         lines = sim_file.readlines()
 
                     for line in lines:
                         if line.startswith('Starting delta mu value (kcal/mol):'):
                             supersat = float(line.split()[-1])
-                            supersaturation.append(supersat)
+                            supersats.append(supersat)
+        print(size_files)
 
-        return supersaturation
+        
+        return (supersats, size_files)
 
-    def find_lengths(self, folders, path, file):
-        lt_df = pd.read_csv(path + '/' + folders[0] + '/' + file[0])
+
+    def find_lengths(self, csv):
+        '''Returns the lenghts from a size_file'''
+        lt_df = pd.read_csv(csv)
         columns = lt_df.columns
-        time = []
-        ratios = []
         lengths = []
         for col in columns:
-            if col.startswith('time'):
-                time.append(col)
             if col.startswith(' '):
                 lengths.append(col)
+
         return lengths
 
-    def calc_growth_rate(self, path, folders, file, lengths, supersaturation):
-        growth_list = []
-        growth_array = np.array(lengths)
-        i = 0
-        for f in folders:
-            lt_df = pd.read_csv(path + '/' + f + '/' + file[i])
-            x_data = lt_df['time']
-            gr_list = []
-            for direction in lengths:
-                print(direction)
-                print(path + '/' + f + '/' + file[i])
-                y_data = np.array(lt_df[direction])
-                model = np.polyfit(x_data, y_data, 1)
-                gr_list.append(model[0])
-                # growth_array = np.append(growth_array, gr_list)
-            growth_list.append(gr_list)
-            # print(growth_list)
-            i += 1
-        growth_array = np.append(growth_array, growth_list)
-        gr_df = pd.DataFrame(growth_list, columns=lengths)
-        gr_df.insert(0, 'Supersaturation', supersaturation)
+    def run_calc_growth(self, path):
+        '''Returns the final df/csv of the 
+        growth rates vs supersaturation'''
 
-        return gr_df
+        savefolder = self.creates_rates_folder(path)
+        supersats, size_files = self.find_info(path)
+
+        print(supersats)
+        print(size_files)
+
+        calc = calculator()
+
+        growth_rate_df = calc.calc_growth_rate(size_files, supersats)
+        growth_rate_df.to_csv(savefolder / 'growthrates.csv')
+        
