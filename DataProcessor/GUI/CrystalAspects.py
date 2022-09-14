ffd59dd34052b65b0b3099288eb8285c8fd84364
AJen01 <71494089+AJen01@users.noreply.github.com>
2022-09-15 05:06:16 +0800

    Growth rate plots

---
+++ DataProcessor/GUI/CrystalAspects.py
@@ -0,0 +1,157 @@
+# Project Modules 
+
+# Project module imports
+from load_GUI import Ui_MainWindow
+
+# PyQT5 imports
+from PyQt5 import QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication, QComboBox, QMainWindow, QMessageBox, QShortcut, QFileDialog
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QKeySequence
+from qt_material import apply_stylesheet
+
+# General imports
+import os, sys, time, subprocess
+import numpy as np
+import pandas as pd
+from pandas import DataFrame
+from collections import defaultdict
+import webbrowser
+from natsort import natsorted
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.setWindowIcon(QtGui.QIcon('icon.png'))
+        apply_stylesheet(app, theme='dark_cyan.xml')
+        self.setupUi(self)
+        self.statusBar().showMessage('CrystalGrower Data Processor v1.0')
+
+        # Welcome msg
+        print('############################################')
+        print('####        CrystalAspects v1.00        ####')
+        print('############################################')
+
+        ######################################################
+        ############      Keyboard Shortcuts      ############
+        ######################################################
+
+        self.openKS = QShortcut(QKeySequence('Ctrl+O'), self)
+        self.openKS.activated.connect(self.readFolder)
+        self.open_outKS = QShortcut(QKeySequence('Ctrl+Shift+O'), self)
+        self.open_outKS.activated.connect(self.output_folder_button)
+        self.closeKS = QShortcut(QKeySequence('Ctrl+Q'), self)
+        self.closeKS.activated.connect(QApplication.instance().quit)
+        self.resetKS = QShortcut(QKeySequence('Ctrl+R'), self)
+        self.resetKS.activated.connect(self.reset_button_function)
+        self.applyKS = QShortcut(QKeySequence('Ctrl+A'), self)
+        self.applyKS.activated.connect(self.apply_clicked)
+
+        self.setWindowIcon(QtGui.QIcon('icon.png'))
+
+        apply_stylesheet(app, theme='dark_cyan.xml')
+
+        # Open Folder
+        try:
+            self.simFolder_Button.clicked.connect(self.read_folder)
+        except IndexError:
+            pass
+
+        # Create a list to store directions being selected
+        checked_facets = []
+
+        # Unlock options
+        self.aspectRatio_checkBox.stateChanged.connect(self.aspect_ratio_checked)
+        self.aspectRatio_checkBox_2.stateChanged.connect(self.aspect_ratio_checked)
+
+        # Collect Variable values
+        self.growthRate_checkBox.stateChanged.connect(self.growth_rate_check)
+        self.growthRate_checkBox_2.stateChanged.connect(self.growth_rate_check)
+        self.plot_checkBox.stateChanged.connect(self.plot_check)
+        self.plot_checkBox_2.stateChanged.connect(self.plot_check)
+        self.count_checkBox.stateChanged.connect(self.count_check)
+        self.aspect_range_checkBox.stateChanged.connect(self.range_check)
+        self.tabWidget.currentChanged.connect(self.tabChanged)
+
+
+        
+
+
+        # Find current tab and sets search_mode
+        
+        #self.tabWidget.setCurrentIndex(0)
+        search_mode = self.tabWidget.currentIndex() + 1
+        
+
+        # Setting post-selection buttons
+        self.apply_button.clicked.connect(self.apply_clicked)
+        self.apply_button_2.clicked.connect(self.apply_clicked)
+        self.outFolder_Button.clicked.connect(self.output_folder_button)
+        self.reset_button.clicked.connect(self.reset_button_function)
+        self.reset_button_2.clicked.connect(self.reset_button_function)
+
+        #Initialize progressBar
+        self.progressBar.setValue(0)
+
+        ######################################################
+        ############       Map/Slider Mode        ############
+        ######################################################
+        
+        self.select_summary_map_button.clicked.connect(self.maps_read_summary)
+        self.select_summary_slider_button.clicked.connect(self.slider_summary)
+        self.generate_map_button.clicked.connect(self.maps_map_function)
+
+        ######################################################
+        ############        3D Data Mode          ############
+        ######################################################
+
+        #self.select_XYZ_3d_button.clicked.connect(self.read_XYZ1)
+        #self.select_sim_3d_button.clicked.connect(self.read_XYZ2)
+        self.start_3dcalc_button.clicked.connect(lambda: self.SAVAR_calc('start'))
+        self.stop_3dcalc_button.clicked.connect(lambda: self.SAVAR_calc('stop'))
+
+
+    def read_folder(self):
+        
+
+
+
+
+# Override sys excepthook to prevent app abortion upon any error
+# (Reverts to old PyQt4 behaviour of 
+# simply printing the traceback to stdout/stderr)
+
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+    
+
+if __name__ == "__main__":
+    import sys
+
+    # Override sys excepthook to prevent app abortion upon any error
+    sys.excepthook = except_hook
+
+
+    # Setting taskbar icon permissions - windows
+    try:
+        import ctypes
+
+        appid = u'CNM.CrystalGrower.DataProcessor.v1.0'  # arbitrary string
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(appid)
+    except:
+        pass
+
+    # ############# Runs the application ############## #
+    QApplication.setAttribute(Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
+    app = QtWidgets.QApplication(sys.argv)
+    mainwindow = MainWindow()
+    mainwindow.show()
+    sys.exit(app.exec_())
+    
+
--- DataProcessor/data/find_data.py
+++ DataProcessor/data/find_data.py
@@ -8,7 +8,7 @@ from DataProcessor.tools.shape_analysis import CrystalShape as cs
 from DataProcessor.data.calc_data import Calculate as calc
 
 
-class Run:
+class Find:
 
     def __init__(self):
         self.method = 0
@@ -19,89 +19,64 @@ class Run:
 
         return aspects_folder
 
-    def build_AR_PCA(self, subfolder):
-        aspects_folder = self.create_aspects_folder(subfolder)
+    def find_info(self, path):
+        '''The method returns the crystallographic directions,
+        supersations, and the size_file paths from a CG simulation folder'''
 
-        final_array = np.empty((0, 6), np.float64)
-        print(final_array.shape)
-
-        for file in Path(subfolder).iterdir():
-            if file.suffix == '.XYZ':
-
-                sim_num = re.findall(r'\d+', Path(file).name)[-1]
-                shape = cs()
-                try:
-                    xyz = shape.read_XYZ()
-                    vals = shape.get_PCA(xyz)
-                    # print(vals)
-
-                    calculator = calc()
-                    ar_data = calculator.aspectratio(pca_vals=vals)
-                    ar_data = np.insert(ar_data, 0, sim_num, axis=1)
-
-                    final_array = np.append(final_array, ar_data, axis=0)
-                    print(final_array.shape)
-
-                except StopIteration:
+        path = Path(path)
+        files = os.listdir(path)
+        contents = natsorted(files)
+        folders = []
+        for item in contents:
+            item_name = item
+            print(item_name)
+            item_path = path / item
+            if os.path.isdir(item_path):
+                if item_name.endswith('XYZ_files') or item_name.endswith('CrystalAspects') \
+                or item_name.endswith('CrystalMaps'):
                     continue
-                except UnicodeDecodeError:
+                else:
+                    folders.append(item_path)
+        print(folders)
+        size_files = []
+        supersats = []
+
+        for folder in folders:
+            files = os.listdir(folder)
+            for f in files:
+                f_path = path / folder / f
+                f_name = f
+                if f_name.startswith("._"):
                     continue
+                if f_name.endswith('size.csv'):
+                    size_files.append(f_path)
+            
+                if f_name.endswith('simulation_parameters.txt'):
+                    with open(f_path, 'r', encoding='utf-8') as sim_file:
+                        lines = sim_file.readlines()
 
-        print(final_array.shape)
+                    for line in lines:
+                        if line.startswith('Starting delta mu value (kcal/mol):'):
+                            supersat = float(line.split()[-1])
+                            supersats.append(supersat)
+        print(size_files)
 
-        # Converting np array to pandas df
-        df = pd.DataFrame(final_array, columns=['Simulation Number',
-                                                'Small',
-                                                'Medium',
-                                                'Long',
-                                                'S:M',
-                                                'M:L'])
-        aspects_folder = Path(subfolder) / 'CrystalAspects'
-        aspect_csv = f'{aspects_folder}/PCA_aspectratio.csv'
-        df.to_csv(aspect_csv)
-
-        return df
-
-    def build_AR_Crystallographic(self, file):
-        filepath = Path(file)
-        df = pd.read_csv(filepath)
         
-        final_array = np.empty((0, 6), np.float64)
-        print(final_array.shape)
+        return (supersats, size_files)
 
-        for index, row in df.iterrows():
-            index =+ 1
-            try:
-                vals = row[1:4].values
-                # print(vals)
 
-                calculator = calc()
-                ar_data = calculator.aspectratio(vals=vals)
-                ar_data = np.insert(ar_data, 0, index, axis=1)
+    def find_lengths(self, csv):
+        '''Returns the lenghts from a size_file'''
+        lt_df = pd.read_csv(csv)
+        columns = lt_df.columns
+        lengths = []
+        for col in columns:
+            if col.startswith(' '):
+                lengths.append(col)
 
-                final_array = np.append(final_array, ar_data, axis=0)
-                print(final_array.shape)
+        return lengths
 
-            except StopIteration:
-                continue
-            except UnicodeDecodeError:
-                continue
-
-        print(final_array.shape)
-
-        # Converting np array to pandas df
-        df = pd.DataFrame(final_array, columns=['Simulation Number',
-                                                'Small',
-                                                'Medium',
-                                                'Long',
-                                                'S:M',
-                                                'M:L'])
-        aspects_folder = filepath.parents[0] / 'CrystalAspects'
-        aspects_folder.mkdir(parents=True, exist_ok=True)
-        aspect_csv = f'{aspects_folder}/PCA_aspectratio.csv'
-        df.to_csv(aspect_csv)
-
-        return df
+    
 
     def growth_rates(self, file):
         # call the growth_rates.py
