deaf4466a5ddf7b30a4ef162e5250d7e3e9fafad
AJen01 <71494089+AJen01@users.noreply.github.com>
2022-09-24 00:03:54 +0800

    file formatted

--- CrystalAspects/GUI/gui_commands.py
+++ CrystalAspects/GUI/gui_commands.py
@@ -7,9 +7,16 @@ from pathlib import Path
 from CrystalAspects.tools.shape_analysis import CrystalShape as cs
 from CrystalAspects.data.calc_data import Calculate as calc
 
-from PyQt5 import QtWidgets, QtGui, QtCore, QtOpenGL 
+from PyQt5 import QtWidgets, QtGui, QtCore, QtOpenGL
 
-from PyQt5.QtWidgets import QApplication, QComboBox, QMainWindow, QMessageBox, QShortcut, QFileDialog
+from PyQt5.QtWidgets import (
+    QApplication,
+    QComboBox,
+    QMainWindow,
+    QMessageBox,
+    QShortcut,
+    QFileDialog,
+)
 from PyQt5.QtCore import Qt, pyqtSignal
 from PyQt5.QtGui import QKeySequence
 
@@ -19,9 +26,7 @@ from load_GUI import Ui_MainWindow
 from CrystalAspects.data.find_data import Find
 
 
-class GUICommands():
-
+class GUICommands:
     def __init__(self, *args, **kwargs):
         super(GUICommands, self).__init__(*args, **kwargs)
         pass
-
--- CrystalAspects/data/calc_data.py
+++ CrystalAspects/data/calc_data.py
@@ -4,7 +4,6 @@ from CrystalAspects.tools.shape_analysis import CrystalShape as cs
 
 
 class Calculate:
-
     def __init__(self):
         pass
 
@@ -12,27 +11,25 @@ class Calculate:
         pass
 
     def aspectratio(self, vals):
-        self.small, self.medium, self.long = (sorted(vals))
-        
+        self.small, self.medium, self.long = sorted(vals)
+
         self.aspect1 = self.small / self.medium
         self.aspect2 = self.medium / self.long
 
-        aspect_array = np.array([[self.small,
-                                self.medium,
-                                self.long,
-                                self.aspect1,
-                                self.aspect2]])
-        
+        aspect_array = np.array(
+            [[self.small, self.medium, self.long, self.aspect1, self.aspect2]]
+        )
+
         return aspect_array
 
     def calc_growth_rate(self, size_file_list, supersat_list, directions=[]):
         growth_list = []
         lengths = []
         i = 0
-            
+
         for f in size_file_list:
             lt_df = pd.read_csv(f)
-            x_data = lt_df['time']
+            x_data = lt_df["time"]
 
             if directions:
                 lengths = directions
@@ -40,10 +37,9 @@ class Calculate:
                 columns = lt_df.columns
                 if i == 0:
                     for col in columns:
-                        if col.startswith(' ') or col.startwith('-1'):
+                        if col.startswith(" ") or col.startwith("-1"):
                             print(col)
                             lengths.append(col)
-                
 
             gr_list = []
             for direction in lengths:
@@ -55,6 +51,6 @@ class Calculate:
             i += 1
         growth_array = np.array(growth_list)
         gr_df = pd.DataFrame(growth_array, columns=lengths)
-        gr_df.insert(0, 'Supersaturation', supersat_list)
+        gr_df.insert(0, "Supersaturation", supersat_list)
 
         return gr_df
