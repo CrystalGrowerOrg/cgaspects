abf87d3d94985999c379c0fce9b8c0064acf00b2
AJen01 <71494089+AJen01@users.noreply.github.com>
2022-10-04 16:00:09 +0800

    removing chmpy

--- CrystalAspects/tools/shape_analysis.py
+++ CrystalAspects/tools/shape_analysis.py
@@ -3,14 +3,10 @@ from pathlib import Path
 import trimesh
 from scipy.spatial import ConvexHull
 from sklearn.decomposition import PCA
-from chmpy.shape.convex_hull import transform_hull
-from chmpy.shape.reconstruct import reconstruct
-from chmpy.shape.sht import SHT
-
 
 class CrystalShape:
-    def __init__(self, l_max=10):
-        self.sht = SHT(l_max)
+    def __init__(self,):
+        pass
 
     def normalise_verts(self, verts):
 
@@ -36,46 +32,12 @@ class CrystalShape:
 
         return self.xyz
 
-    def get_coeffs(self, xyz_vals):
-
-        self.xyz = xyz_vals
-        self.hull = ConvexHull(self.normalise_verts(self.xyz))
-        self.coeffs = transform_hull(self.sht, self.hull)
-
-        return self.coeffs
-
-    def reference_shape(self, shapepath):
-
-        if Path(shapepath).suffix == ".XYZ":
-            self.xyz = self.read_XYZ(shapepath)
-            self.coeffs = self.get_coeffs(self.xyz)
-
-        else:
-            mesh = trimesh.load(shapepath)
-            norm_verts = self.normalise_verts(mesh.vertices)
-            self.stl_hull = ConvexHull(norm_verts)
-            self.coeffs = transform_hull(self.sht, self.stl_hull)
-
-        return self.coeffs
-
-    def compare_shape(self, ref_coeffs, shape_coeffs):
-        self.distance = np.linalg.norm(ref_coeffs - shape_coeffs)
-
-        return self.distance
-
     def get_PCA(self, xyz_vals, filetype=".XYZ", n=3):
         pca = PCA(n_components=n)
 
         if filetype == ".XYZ" or ".xyz":
             pca.fit(self.normalise_verts(xyz_vals))
-
-        if filetype == ".stl":
-            norm_verts = self.normalise_verts(xyz_vals.vertices)
-            self.stl_hull = ConvexHull(norm_verts)
-            self.coeffs = transform_hull(self.sht, self.stl_hull)
-            self.points = list(reconstruct(coefficients=self.coeffs))
-            pca.fit(self.points)
-
+        
         # pca_vectors = pca.components_
         # pca_values = pca.explained_variance_ratio_
         pca_svalues = pca.singular_values_
@@ -116,26 +78,4 @@ class CrystalShape:
 
         shape_info = np.array([[aspect1, aspect2, SA_hull, vol_hull, sa_vol]])
 
-        """shape_info = {
-                "Aspect Ratio S:M": aspect1,
-                "Aspect Ratio M:L": aspect2,
-                "Surface Area (SA)": SA_hull,
-                "Volume (Vol)": vol_hull,
-                "SA : Vol": sa_vol
-                }"""
-
         return shape_info
-
-    def coeff_to_xyz(self, coeffs, path=".", index=0, name="", write_txt=False):
-
-        self.points = list(reconstruct(coefficients=coeffs))
-
-        if write_txt:
-            n_points = len(self.points)
-            filepath = Path(path) / f"xyz_{name}_{index}.txt"
-            with open(filepath, "w") as xyz_file:
-                xyz_file.write(f"{n_points}\n{filepath}\n")
-                for line in self.points:
-                    xyz_file.write(f"{line[0]}  {line[1]}  {line[2]}\n")
-
-        return self.points
