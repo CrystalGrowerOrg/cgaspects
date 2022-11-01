312406db4ca7dc4e4a89b8adf1217b347196abe0
NathandB94 <84033849+NathandB94@users.noreply.github.com>
2022-11-01 09:54:56 +0000

    update replotting

--- CrystalAspects/visualisation/replotting.py
+++ CrystalAspects/visualisation/replotting.py
@@ -25,8 +25,19 @@ class Replotting:
         PCA + Int 
         """
 
-    def replot_GrowthRate(self, csv):
-        pass
+    def replot_GrowthRate(self, csv, info, selected, savepath):
+        print(csv, info, selected)
+        gr_df = pd.read_csv(csv)
+        x_data = gr_df["Supersaturation"]
+        print(selected)
+        for i in selected:
+            plt.scatter(x_data, gr_df[i], s=1.2)
+            plt.plot(x_data, gr_df[i], label=i)
+            plt.legend()
+            plt.xlabel("Supersaturation (kcal/mol)")
+            plt.ylabel("Growth Rate")
+            plt.tight_layout()
+        plt.savefig(savepath / "Growth_rates+Dissolution_rates", dpi=300)
 
     def replot_SAVAR(self, csv):
         pass
