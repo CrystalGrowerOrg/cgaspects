8242bca1cf0ce0a5b279379eb4e4ef40efff5595
AJen01 <71494089+AJen01@users.noreply.github.com>
2024-01-22 04:51:52 +0800

    new plotting - working for aspect rations

--- src/crystalaspects/gui/widgets/plot_axes_widget.py
+++ src/crystalaspects/gui/widgets/plot_axes_widget.py
@@ -2,13 +2,24 @@ from PySide6.QtWidgets import (
     QWidget,
     QVBoxLayout,
     QHBoxLayout,
+    QGridLayout,
     QLabel,
     QComboBox,
     QPushButton,
+    QListView,
 )
 from PySide6.QtCore import Qt, Signal
 from PySide6 import QtCore
 from PySide6 import QtGui
+
+
+class CheckableView(QListView):
+    def mousePressEvent(self, event):
+        self.clearSelection()
+        QListView.mousePressEvent(self, event)
+        self.setSelection(
+            self.rectForIndex(self.currentIndex()), QtCore.QItemSelectionModel.Select
+        )
 
 
 class CheckableComboBox(QComboBox):
@@ -19,7 +30,12 @@ class CheckableComboBox(QComboBox):
         super(CheckableComboBox, self).__init__()
         self._model = QtGui.QStandardItemModel(self)
         self.setModel(self._model)
-        self.view().pressed.connect(self.handleItemPressed)
+
+        self._view = CheckableView(self)
+        self._view.setModel(self._model)
+        self.setView(self._view)
+
+        self._view.pressed.connect(self.handleItemPressed)
 
     def addItem(self, text, data=None):
         item = QtGui.QStandardItem()
@@ -34,13 +50,14 @@ class CheckableComboBox(QComboBox):
             self.addItem(text)
 
     def handleItemPressed(self, index):
+        # Handle item check state
         item = self._model.itemFromIndex(index)
-        if item.checkState() == Qt.Checked:
-            item.setCheckState(Qt.Unchecked)
-        else:
-            item.setCheckState(Qt.Checked)
-        self.itemCheckedStateChanged.emit(index.row(), item.checkState() == Qt.Checked)
-        self.updateDisplayText()
+        newState = Qt.Unchecked if item.checkState() == Qt.Checked else Qt.Checked
+        item.setCheckState(newState)
+        self.itemCheckedStateChanged.emit(index.row(), newState == Qt.Checked)
+
+        # Keep the combobox open
+        self.showPopup()
 
     def checkedItems(self):
         checked_items = []
@@ -63,44 +80,69 @@ class PlotAxesComboBoxes(QWidget):
         layout = QVBoxLayout(self)
 
         # Layout for combo boxes and their labels
-        combo_layout = QHBoxLayout()
-        layout.addLayout(combo_layout)
+        # combo_layout = QHBoxLayout()
+        # layout.addLayout(combo_layout)
+
+        # # X-axis ComboBox
+        # self.x_axis_label = QLabel("X-axis:", self)
+        # self.x_axis_label.setAlignment(Qt.AlignRight)
+        # self.x_axis_combobox = QComboBox(self)
+
+        # combo_layout.addWidget(self.x_axis_label)
+        # combo_layout.addWidget(self.x_axis_combobox)
+
+        # # Y-axis ComboBox (ListView with checkboxes)
+        # self.y_axis_label = QLabel("Y-axis:", self)
+        # self.y_axis_label.setAlignment(Qt.AlignRight)
+        # self.y_axis_combobox = CheckableComboBox()
+        # combo_layout.addWidget(self.y_axis_label)
+        # combo_layout.addWidget(self.y_axis_combobox)
+
+        # # Color ComboBox
+        # self.color_label = QLabel("Custom Coloring:", self)
+        # self.color_label.setAlignment(Qt.AlignRight)
+        # self.color_combobox = QComboBox(self)
+        # self.color_combobox.addItems(["None"])
+        # combo_layout.addWidget(self.color_label)
+        # combo_layout.addWidget(self.color_combobox)
+
+        # # Set the main layout
+        # self.setLayout(layout)
+
+        grid = QGridLayout()
+        layout.addLayout(grid)
 
         # X-axis ComboBox
         self.x_axis_label = QLabel("X-axis:", self)
         self.x_axis_combobox = QComboBox(self)
-
-        combo_layout.addWidget(self.x_axis_label)
-        combo_layout.addWidget(self.x_axis_combobox)
+        grid.addWidget(self.x_axis_label, 0, 0)
+        grid.addWidget(self.x_axis_combobox, 0, 1)
 
         # Y-axis ComboBox (ListView with checkboxes)
         self.y_axis_label = QLabel("Y-axis:", self)
         self.y_axis_combobox = CheckableComboBox()
-        combo_layout.addWidget(self.y_axis_label)
-        combo_layout.addWidget(self.y_axis_combobox)
+        grid.addWidget(self.y_axis_label, 0, 2)
+        grid.addWidget(self.y_axis_combobox, 0, 3)
 
         # Color ComboBox
-        self.color_label = QLabel("Color:", self)
+        self.color_label = QLabel("Custom Coloring:", self)
         self.color_combobox = QComboBox(self)
-        combo_layout.addWidget(self.color_label)
-        combo_layout.addWidget(self.color_combobox)
+        self.color_combobox.addItems(["None"])
+        grid.addWidget(self.color_label, 0, 4)
+        grid.addWidget(self.color_combobox, 0, 5)
 
-        # Plot button
-        self.plot_button = QPushButton("Plot")
-        combo_layout.addWidget(self.plot_button)
+        # Set alignment to right for all labels and combo boxes
+        for i in range(grid.rowCount()):
+            for j in range(grid.columnCount()):
+                widget = grid.itemAtPosition(i, j).widget()
+                if widget:
+                    grid.setAlignment(widget, Qt.AlignRight)
 
-        # Set the main layout
         self.setLayout(layout)
 
     def get_selections(self):
         x_selection = self.x_axis_combobox.currentText()
-
-        y_selections = []
-        for index in range(self.y_axis_model.rowCount()):
-            item = self.y_axis_model.item(index)
-            if item.checkState() == Qt.Checked:
-                y_selections.append(item.text())
-
+        y_selections = self.y_axis_combobox.checkedItems()
         color_selection = self.color_combobox.currentText()
 
         return x_selection, y_selections, color_selection
