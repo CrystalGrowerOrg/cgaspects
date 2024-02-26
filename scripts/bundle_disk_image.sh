2ca3743aac62359e20923bea0e3714d795b528a5
Peter Spackman <peterspackman@fastmail.com>
2024-02-26 15:05:31 +0800

    Update packaging to bundle into disk image

---
+++ scripts/bundle_disk_image.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Variables
+FOLDER_PATH="CGAspects-MacOS/"
+APP_PATH="dist/CGAspects.app"
+DISK_IMAGE_PATH="CGAspects-MacOS.dmg"
+VOLUME_NAME="CGAspects"
+
+# Create the folder structure
+mkdir -p "$FOLDER_PATH"
+
+# Copy the application
+cp -r "$APP_PATH" "$FOLDER_PATH/"
+
+# Create a symlink to the Applications folder
+ln -s /Applications "$FOLDER_PATH/Applications"
+
+# Create the disk image
+hdiutil create -volname "$VOLUME_NAME" -srcfolder "$FOLDER_PATH" -ov -format UDZO "$DISK_IMAGE_PATH"
+
+rm -r "$FOLDER_PATH"
