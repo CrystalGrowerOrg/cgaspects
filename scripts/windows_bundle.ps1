9c4676f4c4a09c9e532bddad61abfdb3ddf44227
Peter Spackman <peterspackman@fastmail.com>
2024-05-24 10:23:07 +0800

    Fix windows bundle script

--- scripts/windows_bundle.ps1
+++ scripts/windows_bundle.ps1
@@ -3,8 +3,8 @@ $ErrorActionPreference = "Stop"
 $ARCH = if ($env:ARCH) { $env:ARCH } else { "x86_64" }
 $PLATFORM = "windows-${ARCH}"
 
-pyinstaller .installer\CrystalAspects.py --onefile --windowed --name=CGAspects `
-    --icon=.res\app_icons\CrystalAspects.png `
+pyinstaller .installer\CGAspects.py --onefile --windowed --name=CGAspects `
+    --icon=.res\app_icons\CGAspects.png `
     --noconfirm --exclude-module=pytest 
 
 Write-Host "Build completed successfully."
