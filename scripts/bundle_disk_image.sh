2debc4b5b871389a29888aff1ad2f0f1eb7db887
Peter Spackman <peterspackman@fastmail.com>
2024-05-24 10:47:38 +0800

    add arch information to macos bundle

--- scripts/bundle_disk_image.sh
+++ scripts/bundle_disk_image.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+set -e
+ARCH="${ARCH:-arm64}"
+export PLATFORM="macos-${ARCH}"
 
 # Variables
-FOLDER_PATH="CGAspects-MacOS/"
+FOLDER_PATH="CGAspects-MacOS-bundle/"
 APP_PATH="dist/CGAspects.app"
-DISK_IMAGE_PATH="CGAspects-MacOS.dmg"
+DISK_IMAGE_PATH="CGAspects-${PLATFORM}.dmg"
 VOLUME_NAME="CGAspects"
 
 # Create the folder structure
