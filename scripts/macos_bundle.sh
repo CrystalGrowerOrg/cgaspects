05b57e6468430bac57af5d545bae48130d227ad7
Peter Spackman <peterspackman@fastmail.com>
2024-05-24 10:18:23 +0800

    Fix package.yml workflow to actually install dependencies

--- scripts/macos_bundle.sh
+++ scripts/macos_bundle.sh
@@ -1,8 +1,21 @@
-pyinstaller installer/CGAspects.py --onedir --windowed --icon=res/app_icons/CGAspects.icns --noconfirm \
+#!/bin/bash
+set -e
+ARCH="${ARCH:-arm64}"
+
+# pyinstaller requires universal2
+ARCH=${ARCH/universal/universal2}
+
+echo "Running pyinstaller"
+pyinstaller installer/CGAspects.py --onedir --windowed \
+    --name=CGAspects \
+    --icon=res/app_icons/CGAspects.icns --noconfirm \
+    --target-architecture="${ARCH}" \
     --exclude-module=pytest
+
 echo "Running ad hoc code signing"
 export APP="dist/CGAspects.app"
 export CONTENTS_DIR="${APP}/Contents"
+
 codesign --force --deep --sign - "${CONTENTS_DIR}/MacOS/CGAspects"
 find "${CONTENTS_DIR}/MacOS" "${CONTENTS_DIR}/Frameworks" \
      "${CONTENTS_DIR}/Resources" \
